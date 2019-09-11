1fd77d6b201c944602e98bca4892b9cb4ece1c71
Baker Renneckar <bakerrenneckar@gmail.com>
2019-09-11 13:47:08 -0700

    init commit

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ import setuptools
 setuptools.setup(
         name="thinkplot",
         version="0.1",
-        packages=setuptools.find_packages()
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'pandas',
+            'numpy',
+            'matplotlib'
+            ]
         )
